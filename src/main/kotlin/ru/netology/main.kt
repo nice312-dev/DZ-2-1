ebe3e7aa57e2024591e727be4f25b3a7cc2b1cb3
nice312-dev <74597002+nice312-dev@users.noreply.github.com>
2020-12-15 13:12:37 +0500

    Update main.kt

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -4,5 +4,5 @@ fun main() {
     val amount = 1000000
     val commission = if (0.0075 * amount < 3500.0) 3500.0 else 0.0075 * amount
 
-    println("You paid: $commission")
+    println("You commission: $commission")
 }
