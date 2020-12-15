5e62ab4bc496b1eac613721647a6eb8829373ad7
nice312-dev <74597002+nice312-dev@users.noreply.github.com>
2020-12-15 13:12:10 +0500

    Update main.kt

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -3,11 +3,6 @@ package ru.netology
 fun main() {
     val amount = 1000000
     val commission = if (0.0075 * amount < 3500.0) 3500.0 else 0.0075 * amount
-    val minAmount = 35
 
-    val totalPrice = amount * commission
-    val result = amount + totalPrice
-    println("Minimum pay: $minAmount")
-    println("You paid: $amount")
-    println("To pay: $result")
+    println("You paid: $commission")
 }
